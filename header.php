7728c02cdf0854c7d42819e52acf6eb833ca16ae
Kyliaann Bourdonnay <kyl.bourdonnay@gmail.com>
2021-01-30 22:31:22 +0100

    Start

---
+++ header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>CV - Yves SKRZYPCZYK</title>
+		<meta name="description" content="Développeur WEB Yves SKRZYPCZYK">
+	</head>
+	<body>
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
-    echo "Coucou";
+    require "header.php";
+    require "footer.php";
 ?>
