7327b01e272a63c79c84862b26f9adc6d3c250a0
Kyliaann Bourdonnay <kyl.bourdonnay@gmail.com>
2021-01-30 22:53:18 +0100

    Start

--- header.php
+++ header.php
@@ -4,6 +4,6 @@
 		<meta charset="UTF-8">
 		<title></title>
 		<meta name="description" content="My Site">
-        <link rel="stylesheet" href="css/cvstyle.css">
+        <link rel="stylesheet" href="css/style.css">
 	</head>
 	<body>
