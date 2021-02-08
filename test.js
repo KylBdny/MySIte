bc73e63a09f157a6285d8e72655767d567cba124
KylBdny <kyl.bourdonnay@gmail.com>
2021-02-08 12:29:23 +0100

    test

---
+++ test.js
@@ -0,0 +1,26 @@
+/* var test = 'test';
+test = 'Faux';
+console.log(test);
+
+if (test === 'test') {
+    console.log("C'est bon")
+}else{
+    console.log("C'est pas bon")
+}; */
+
+/* let athleteFinalPosition = 'first place';
+
+switch(athleteFinalPosition){
+  case 'first place':
+    console.log('You get the gold medal!');
+    break;
+  case 'second place':
+    console.log('You get the silver medal!');
+    break;
+  case 'third place':
+    console.log('You get the bronze medal!');
+    break;
+  default:
+    console.log('No medal awarded.');
+    break;
+} */
